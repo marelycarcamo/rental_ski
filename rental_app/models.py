8fac5dc4be7d9b317d417468892d063567bf0153
marely <marely.carcamo@gmail.com>
2026-02-06 21:33:35 -0300

    update python 3.14

--- rental_app/models.py
+++ rental_app/models.py
@@ -1,25 +1,23 @@
-from django.db import models
-from django.contrib.auth.models import User
+import django.db.models
+import django.contrib.auth.models
 
-class Usuario(models.Model):
+class Usuario(django.db.models.Model):
     # Relación one-to-one con el modelo User de Django
-    user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
-    created = models.DateTimeField(auto_now_add=True)
+    user = django.db.models.OneToOneField(django.contrib.auth.models.User, on_delete=django.db.models.DO_NOTHING)
+    created = django.db.models.DateTimeField(auto_now_add=True)
     
     # Opciones para el campo 'tipo'
     TIPO_USUARIO = [
         ('operario', 'Operario'),
         ('cliente', 'Cliente'),
     ]
-    tipo = models.CharField(max_length=12, choices=TIPO_USUARIO)
+    tipo = django.db.models.CharField(max_length=12, choices=TIPO_USUARIO)
     
     def __str__(self):
         return f'{self.user} ({self.tipo})'
 
-    
-
 # Modelo de Categoría
-class Categoria(models.Model):
+class Categoria(django.db.models.Model):
     CATEGORIA = [
         ('ski', 'Ski'),
         ('botas', 'Botas'),
@@ -27,35 +25,35 @@ class Categoria(models.Model):
         ('snowboard', 'Snowboard'),
         ('otros', 'Otros'),
     ]
-    nombre = models.CharField(max_length=255, choices=CATEGORIA)
+    nombre = django.db.models.CharField(max_length=255, choices=CATEGORIA)
 
     def __str__(self):
         return self.nombre
 
 # Modelo de Equipo
-class Equipo(models.Model):
-    nombre = models.CharField(max_length=255)
-    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
-    imagen = models.URLField(max_length=200)
-    precio = models.IntegerField(default=0)
+class Equipo(django.db.models.Model):
+    nombre = django.db.models.CharField(max_length=255)
+    categoria = django.db.models.ForeignKey(Categoria, on_delete=django.db.models.DO_NOTHING)
+    imagen = django.db.models.URLField(max_length=200)
+    precio = django.db.models.IntegerField(default=0)
     # Opciones para el campo 'estado'
     ESTADO = [
         ('disponible', 'Disponible'),
         ('arrendado', 'Arrendado'),
         ('mantencion', 'Mantencion'),
     ]
-    estado = models.CharField(max_length=45, choices=ESTADO)
+    estado = django.db.models.CharField(max_length=45, choices=ESTADO)
 
     def __str__(self):
         return f'{self.nombre} ({self.estado})'
 
 # Modelo de Arriendo
-class Arriendo(models.Model):
-    user = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
-    equipo = models.ForeignKey(Equipo, on_delete=models.DO_NOTHING)
-    fecha = models.DateTimeField()
-    observacion = models.TextField(blank=True, null=True, default='Sin observaciones')
-    danado = models.BooleanField(default=False)
+class Arriendo(django.db.models.Model):
+    user = django.db.models.ForeignKey(Usuario, on_delete=django.db.models.DO_NOTHING)
+    equipo = django.db.models.ForeignKey(Equipo, on_delete=django.db.models.DO_NOTHING)
+    fecha = django.db.models.DateTimeField()
+    observacion = django.db.models.TextField(blank=True, null=True, default='Sin observaciones')
+    danado = django.db.models.BooleanField(default=False)
     
 
     def __str__(self):
